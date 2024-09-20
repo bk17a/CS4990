9a23d22bb3aaad92c362835dd302ae69da8c9642
Brian Khuu <briankhuu17@gmail.com>
2024-09-20 16:28:28 -0700

    cleaned up code

--- data_collection.py
+++ data_collection.py
@@ -14,49 +14,47 @@ def get_movie_data(title):
         return None
 
 
+def process_movie_data(data):
+    rotten_tomatoes_rating = "N/A"
+    metacritic_rating = "N/A"
+
+    for rating in data.get("Ratings", []):
+        if rating["Source"] == "Rotten Tomatoes":
+            rotten_tomatoes_rating = rating["Value"]
+        elif rating["Source"] == "Metacritic":
+            metacritic_rating = rating["Value"]
+
+    return {
+        "Title": data.get("Title"),
+        "Length": data.get("Runtime"),
+        "Year": data.get("Year"),
+        "Rated": data.get("Rated"),
+        "Released": data.get("Released"),
+        "Genre": data.get("Genre"),
+        "RottenTomatoesRating": rotten_tomatoes_rating,
+        "MetacriticRating": metacritic_rating,
+        "imdbRating": data.get("imdbRating"),
+        "imdbVotes": data.get("imdbVotes"),
+        "imdbID": data.get("imdbID"),
+        "BoxOffice": data.get("BoxOffice"),
+        "Awards": data.get("Awards") if data.get("Awards") != "N/A" else "No Awards",
+        "Director": data.get("Director"),
+        "Writer": data.get("Writer"),
+        "Actors": data.get("Actors"),
+        "Plot": data.get("Plot"),
+        "Language": data.get("Language"),
+        "Country": data.get("Country"),
+        "Type:": data.get("Type"),
+    }
+
+
 def get_dataset(filename, rows=None):
     movie_data = []
 
     for title in movie_titles:
         data = get_movie_data(title)
         if data:
-            rotten_tomatoes_rating = "N/A"
-            metacritic_rating = "N/A"
-
-            for rating in data.get("Ratings", []):
-                if rating["Source"] == "Rotten Tomatoes":
-                    rotten_tomatoes_rating = rating["Value"]
-                elif rating["Source"] == "Metacritic":
-                    metacritic_rating = rating["Value"]
-
-            movie_data.append(
-                {
-                    "Title": data.get("Title"),
-                    "Length": data.get("Runtime"),
-                    "Year": data.get("Year"),
-                    "Rated": data.get("Rated"),
-                    "Released": data.get("Released"),
-                    "Genre": data.get("Genre"),
-                    "RottenTomatoesRating": rotten_tomatoes_rating,
-                    "MetacriticRating": metacritic_rating,
-                    "imdbRating": data.get("imdbRating"),
-                    "imdbVotes": data.get("imdbVotes"),
-                    "imdbID": data.get("imdbID"),
-                    "BoxOffice": data.get("BoxOffice"),
-                    "Awards": (
-                        data.get("Awards")
-                        if data.get("Awards") != "N/A"
-                        else "No Awards"
-                    ),
-                    "Director": data.get("Director"),
-                    "Writer": data.get("Writer"),
-                    "Actors": data.get("Actors"),
-                    "Plot": data.get("Plot"),
-                    "Language": data.get("Language"),
-                    "Country": data.get("Country"),
-                    "Type:": data.get("Type"),
-                }
-            )
+            movie_data.append(process_movie_data(data))
 
         if rows and len(movie_data) >= rows:
             break
